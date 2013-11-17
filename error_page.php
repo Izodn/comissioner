763bb8339c69d5ae0ce90594f5a4a0df5a393e88
Izodn <izodn0@gmail.com>
2013-11-17 15:24:33 -0800

    Starting complete rewrite for v5.0

--- error_page.php
+++
@@ -1,5 +0,0 @@
-<?php
-	echo "An unexpected error has occurred.\n<br>\n";
-	echo "The engineers have been notified.\n<br>\n";
-	echo "Click <a href=\"/clientLogin.php\">here</a> to go back to login.\n<br>\n";
-?>
--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-	require_once $_SERVER['DOCUMENT_ROOT'].'/_/includeList.php';
-	echo '<center>';
-	echo echoClientLinks();
-	echo '<br>';
-	echoClientTable();
-	echo '</center>';
+	
 ?>
