746e62e74aebbbb2c0dec87f5e147fe9fc359b2c
Izodn <izodn0@gmail.com>
2013-11-17 21:13:31 -0800

    Added account creation, and login.

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-
+	require_once $_SERVER['DOCUMENT_ROOT'].'/_/include/class/user.php';
+	session_start();
+	if( !isset($_SESSION['userObj']) ) //If not logged in
+		header('Location: login.php');
 ?>
---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+	require_once $_SERVER['DOCUMENT_ROOT'].'/_/include/class/user.php';
+	if( isset($_POST['login']) ) {
+		if( empty($_POST['username']) || empty($_POST['password']) ) {
+			$errMsg = "Please fill out all fields";
+		}
+		if(!isset($_SESSION))
+			session_start();
+		$userObj = new user($_POST['username'], $_POST['password']);
+		if(!$userObj->doLogin()) { //Will return false if bad can't login
+			$errMsg = $userObj->errMsg;
+		}
+		else {
+			$_SESSION['userObj'] = $userObj;
+			header('Location: index.php'); //Successfully logged in, goto index.
+		}
+	}
+	elseif(isset($_POST['register'])) {
+		header('Location: register.php');
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Commissioner - Login</title>
+	</head>
+	<body>
+		<center>
+			<h3>Login</h3>
+			<form action="<?php echo htmlentities($_SERVER['REQUEST_URI']); ?>" method="POST">
+				<table>
+					<tr>
+						<td>Username: </td>
+						<td><input type="text" name="username"></td>
+					</tr>
+					<tr>
+						<td>Password: </td>
+						<td><input type="password" name="password"></td>
+					</tr>
+					<tr>
+						<td><input type="submit" name="login" value="Login"></td>
+						<td><input type="submit" name="register" value="Register"></td>
+					</tr>
+				</table>
+			</form>
+			<?php echo ($errMsg = isset($errMsg) ? '<font color="#FF0000">'.$errMsg.'</font>' : "")."\n"; ?>
+		</center>
+	<body>
+</html>
